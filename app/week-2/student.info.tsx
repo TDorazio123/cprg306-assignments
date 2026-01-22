4dde8e5edafb23f811aa6c21bbcaa40ea54b710b
TDorazio123 <thomas.dorazio@edu.sait.ca>
2026-01-22 14:39:45 -0700

    Test 1

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Student name: Thomas Dorazio</h1>
+            <Link href="https://github.com/TDorazio123/cprg306-assignments.git">My Github</Link>
+        </main>
+    );
+}
