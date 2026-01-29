b7b43b40356cc260f7f0ec57d064d169f9041a54
TDorazio123 <thomas.dorazio@edu.sait.ca>
2026-01-29 14:08:05 -0700

    Week 3 commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <h2>Welcome to the assignments page!</h2>
+      <Link href="/week-2">Week 2 Assignment</Link>
+      <br />
+      <Link href="/week-3">Week 3 Assignment</Link>
     </main>
   );
 }
